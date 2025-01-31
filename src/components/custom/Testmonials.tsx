6348ae26e3fdbecfd695a128b658b6c985a47f61
Shubham Liman <shubhamlstudymail@gmail.com>
2025-01-31 19:47:34 +0530

    Final changes

---
+++ src/components/custom/Testmonials.tsx
@@ -0,0 +1,74 @@
+import ReviewCard from "./Testmonialcard";
+import person from "../../assets/Ellipse 1.svg";
+import person2 from "../../assets/Ellipse 1 (1).svg";
+import person3 from "../../assets/Ellipse 1 (2).svg";
+import person4 from "../../assets/blackwomen.svg";
+import bg from "../../assets/Group 39875.svg";
+
+function Testmonials() {
+  const ppl = [
+    {
+      profile: person,
+      name: "Wade Warren",
+      role: "UI/UX Designer @Microsoft",
+      comment: "Awesome website and funnel for your business",
+    },
+    {
+      profile: person4,
+      name: "Nicole Champlin",
+      role: "UI/UX Designer @Microsoft",
+      comment: "Couldn’t agree more to this product! ",
+    },
+    {
+      profile: person2,
+      name: "Elias Watsica",
+      role: "UI/UX Designer @Microsoft",
+      comment: "Super recommended product. You have to try!",
+    },
+    {
+      profile: person3,
+      name: "Theresa Conroy",
+      role: "UI/UX Designer @Microsoft",
+      comment: "I cannot believe my eyes, this is real!",
+    },
+  ];
+
+  return (
+    <div className="my-[120px] flex flex-col items-center gap-[60px]">
+      <div className="flex w-[372.8px] p-[17.077px] items-center gap-[27.323px] rounded-[6.831px] bg-[#FFF] [box-shadow:34.154px_23.908px_68.308px_0px_rgba(0,_0,_0,_0.08)]">
+        <div className="flex flex-row gap-[3.42px] items-center">
+          <div className="flex w-[33.727px] h-[33.727px] p-[3.415px] justify-center items-center gap-[17.077px] bg-[#00B57A]">
+            <img src={bg} alt="star" />
+          </div>
+          <div className="flex w-[33.727px] h-[33.727px] p-[3.415px] justify-center items-center gap-[17.077px] bg-[#00B57A]">
+            <img src={bg} alt="star" />
+          </div>
+          <div className="flex w-[33.727px] h-[33.727px] p-[3.415px] justify-center items-center gap-[17.077px] bg-[#00B57A]">
+            <img src={bg} alt="star" />
+          </div>
+          <div className="flex w-[33.727px] h-[33.727px] p-[3.415px] justify-center items-center gap-[17.077px] bg-[#00B57A]">
+            <img src={bg} alt="star" />
+          </div>
+          <div className="flex w-[33.727px] h-[33.727px] p-[3.415px] justify-center items-center gap-[17.077px] bg-[#00B57A]">
+            <img src={bg} alt="star" />
+          </div>
+        </div>
+        <div className="text-[#868686] font-[Manrope] text-[26px] not-italic font-bold leading-[25.616px]">
+          5.0 Rating
+        </div>
+      </div>
+      <div className="flex justify-center items-center gap-[45px]">
+        {ppl.map((person) => (
+          <ReviewCard
+            profile={person.profile}
+            name={person.name}
+            role={person.role}
+            comment={person.comment}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Testmonials;
